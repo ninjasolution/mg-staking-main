0b0e4d590717ef90d17de9b562416eb50434597c
Strong developer <fadeevdoma@gmail.com>
2022-10-13 17:07:24 +0200

    Update NFTDeatilModal.jsx

--- src/modals/NFTDeatilModal.jsx
+++ src/modals/NFTDeatilModal.jsx
@@ -258,7 +258,7 @@ const NFTDeatilModal = ({
                       <tr>                      
                         <td>{data.gameName}</td>
                         <td>{data.tournament}</td>
-                        <td>{data.date.replace('T','  ').substring(0, data.date.indexOf('.'))}</td>
+                        <td>{(new Date(`${data.date}Z`).toLocaleString('en-US', { hour12: false }).substring(0, data.date.lastIndexOf(':')))}</td>
                         <td>{parseFloat(data.percent.substring(0, data.percent.length - 1)).toFixed(2) + '%'}</td>                      
                         <td>{data.tokens}</td>
                       </tr>
